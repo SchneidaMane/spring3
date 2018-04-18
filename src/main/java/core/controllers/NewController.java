0a171ae785dab82618e0db82cac1ae0e9f8112bb
SchneidaMane <schneida@live.unc.edu>
2018-04-17 20:40:35 -0400

    Controller work
    Fixed static handling for for thymeleaf and created flexibil controller
    that can serve any page

--- src/main/java/core/controllers/NewController.java
+++ src/main/java/core/controllers/NewController.java
@@ -1,13 +1,18 @@
 package main.java.core.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/dashboard/")
 public class NewController {
-	@RequestMapping("/")
-	public String home() {
-		return "/pages/index.html";
+	@RequestMapping("/*")
+	public String defaultHandler(HttpServletRequest request) {
+		String servletPath = request.getServletPath();
+		String trimmedPath = servletPath.replaceAll("/dashboard/", "");
+		String templatePath = "/pages/" + trimmedPath;
+		return templatePath;
 	}
 }
